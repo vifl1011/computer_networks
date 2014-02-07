476d14c86e586884f59ae576319daf300d534cdb
vifl1011 <vifl1011@hs-karlsruhe.de>
2014-02-08 00:11:15 +0100

    Messages per second working. 0 value results in one message.

--- 1dv201/src/UDPEchoClient.java
+++ 1dv201/src/UDPEchoClient.java
@@ -4,41 +4,43 @@
  */
 
 //package dv201.labb2;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class UDPEchoClient {
 	public static final int BUFSIZE = 1024;
 	public static final int MYPORT = 0;
-	static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	static final String charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	static Random random = new Random();
+	static BufferedReader bufferedReader;
 
 	public static void main(String[] args) throws IOException {
 		byte[] buf = null;
 		String msg = null;
+		int msgPerSecond = 0;
 		try {
 			buf = new byte[Integer.valueOf(args[2])];
 			msg = randomString(Integer.valueOf(args[3]));
+			msgPerSecond = Integer.valueOf(args[4]);
+
 		} catch (NumberFormatException ex) {
 			System.err.printf("usage: %s server_name port buffer_size\n",
 					args[2]);
 			System.exit(1);
 		}
-		if (args.length != 2) {
-			System.err.printf("usage: %s server_name port\n", args[1]);
-			System.exit(1);
-		}
 
 		/* Create socket */
-		DatagramSocket socket = new DatagramSocket(null);
+		DatagramSocket datagramSocket = new DatagramSocket(null);
 
 		/* Create local endpoint using bind() */
 		SocketAddress localBindPoint = new InetSocketAddress(MYPORT);
-		socket.bind(localBindPoint);
+		datagramSocket.bind(localBindPoint);
 
 		/* Create remote endpoint */
 		SocketAddress remoteBindPoint = new InetSocketAddress(args[0],
@@ -50,10 +52,46 @@ public class UDPEchoClient {
 
 		/* Create datagram packet for receiving echoed message */
 		DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
+		if (msgPerSecond > 1) {
+			do {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					System.err.printf(e.getStackTrace().toString());
+				}
 
-		/* Send and receive message */
-		socket.send(sendPacket);
-		socket.receive(receivePacket);
+				/* Send and receive message */
+
+				for (int it = 0; it < msgPerSecond; it++) {
+					sendReceive(datagramSocket, msg, sendPacket, receivePacket);
+				}
+				System.out.printf("---------------------------------------------------\n");
+
+			} while (true);
+		} else {
+			sendReceive(datagramSocket, msg, sendPacket, receivePacket);
+			System.out.printf("-------------send 1 and done--------------\n");
+		}
+
+		// socket.close();
+	}
+
+	public static String randomString(int length) {
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < length; i++)
+			sb.append(charSet.charAt(random.nextInt(charSet.length())));
+		return sb.toString();
+	}
+
+	public static void sendReceive(DatagramSocket socket, String msg,
+			DatagramPacket sendPacket, DatagramPacket receivePacket) {
+		try {
+			socket.send(sendPacket);
+			socket.receive(receivePacket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		/* Compare sent and received message */
 		String receivedString = new String(receivePacket.getData(),
@@ -63,13 +101,5 @@ public class UDPEchoClient {
 					receivePacket.getLength());
 		else
 			System.out.printf("Sent and received msg not equal!\n");
-		socket.close();
-	}
-
-	public static String randomString(int length) {
-		StringBuilder sb = new StringBuilder(length);
-		for (int i = 0; i < length; i++)
-			sb.append(AB.charAt(random.nextInt(AB.length())));
-		return sb.toString();
 	}
 }
