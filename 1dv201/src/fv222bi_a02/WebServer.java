14d997ff1b5398b35a8a7c128dfae42e3d1f3835
vifl1011 <vifl1011@hs-karlsruhe.de>
2014-02-23 20:24:35 +0100

    Finale version

--- 1dv201/src/fv222bi_a02/WebServer.java
+++ 1dv201/src/fv222bi_a02/WebServer.java
@@ -13,24 +13,18 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URLDecoder;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Date;
 import java.util.StringTokenizer;
 
-public class WebServer extends Thread {
-	static final Path DEFAULT_DIR = Paths.get("E:\\develop\\tmp");
-	static final String DEFAULT_FILE = "index.html";
-	static final int DEFAULT_PORT = 6666;
+public class WebServer implements Runnable {
+	static final File DEFAULT_DIR = new File("E:\\develop\\tmp");
+	static final String DEFAULT_HTML = "index.html";
+	static final int DEFAULT_PORT = 8080;
 
-	public Socket socket;
-	BufferedReader bufferedReader; // input from client
-	PrintWriter printWriter; // sending to client
-	BufferedOutputStream bufferedOutputStream;
-	String fileRequested;
+	public Socket socket;	
 
 	public WebServer(Socket socket) {
 		if (socket == null)
@@ -39,91 +33,79 @@ public class WebServer extends Thread {
 	}
 
 	public void run() {
-		bufferedReader = null;
-		printWriter = null;
-		bufferedOutputStream = null;
+		/* input from client */
+		BufferedReader bufferedReader = null;
+		/* sending to client */
+		PrintWriter printWriter = null;
+		BufferedOutputStream bufferedOutputStream = null;
+		String requestedFile = null;
 
 		try {
-			/* get character input stream from client */
+			/* get input from client */
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			/* get character output stream to client (for headers) */
+			/* get output to client - headers */
 			printWriter = new PrintWriter(socket.getOutputStream());
-			/* get binary output stream to client (for requested data) */
+			/* get binary output to client - data */
 			bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
 
-			/* get first line of request from client */
+			/* read the request of the client */
 			String input = bufferedReader.readLine();
-			/* create StringTokenizer to parse request */
 			StringTokenizer parse = new StringTokenizer(input);
-			/* parse out method */
+			/* parse out http method */
 			String method = parse.nextToken().toUpperCase();
-			/* parse out file requested */
-			fileRequested = parse.nextToken().toLowerCase();
-
-			/* methods other than GET and HEAD are not implemented */
-			if (!method.equals("GET") && !method.equals("HEAD")) {
-				/* send Not Implemented message to client */
-				printWriter.println("HTTP/1.0 501 Not Implemented");
-				printWriter.println("Server: Java HTTP Server 1.0");
-				printWriter.println("Date: " + new Date());
-				printWriter.println("Content-Type: text/html");
-				printWriter.println();
-				printWriter.println("<HTML>");
-				printWriter.println("<HEAD><TITLE>Not Implemented</TITLE> </HEAD>");
-				printWriter.println("<BODY>");
-				printWriter.println("<H2>501 Not Implemented: " + method + " method.</H2>");
-				printWriter.println("</BODY></HTML>");
-				printWriter.flush();
-
-				return;
+			/* parse out name of requested file */
+			requestedFile = parse.nextToken().toLowerCase();
+			
+			/* on root level request give back default page */
+			if (requestedFile.endsWith("/")) {
+				requestedFile += DEFAULT_HTML;
 			}
 
-			// If we get to here, request method is GET or HEAD
-			if (fileRequested.endsWith("/")) {
-				// append default file name to request
-				fileRequested += DEFAULT_FILE;
-			}
-
-			// create file object
-			File file = new File(DEFAULT_DIR.toString(), fileRequested);
-			// get length of file
+			/* create a new File to serve the client */
+			File file = new File(DEFAULT_DIR.toString(), requestedFile);
 			int fileLength = (int) file.length();
 
-			// get the file's MIME content type
-			String content = getContentType(fileRequested);
+			/* get MIME type */
+			String content = getContentType(requestedFile);
 
-			// if request is a GET, send the file content
 			if (method.equals("GET")) {
-				FileInputStream fileIn = null;
-				// create byte array to store file data
+				/* handle GET request by creating new byte array */
+				FileInputStream fileInputStream = null;
 				byte[] fileData = new byte[fileLength];
 
 				try {
-					// open input stream from file
-					fileIn = new FileInputStream(file);
-					// read file into byte array
-					fileIn.read(fileData);
+					/* write file to array */
+					fileInputStream = new FileInputStream(file);
+					fileInputStream.read(fileData);
 				} finally {
-					close(fileIn); // close file input stream
+					close(fileInputStream);
 				}
 
-				// send HTTP headers
 				printWriter.println("HTTP/1.0 200 OK");
 				printWriter.println("Server: Java HTTP Server 1.0");
 				printWriter.println("Date: " + new Date());
 				printWriter.println("Content-type: " + content);
 				printWriter.println("Content-length: " + file.length());
-				printWriter.println(); // blank line between headers and content
-				printWriter.flush(); // flush character output stream buffer
-
-				bufferedOutputStream.write(fileData, 0, fileLength); // write
-																		// file
-				bufferedOutputStream.flush(); // flush binary output stream
-												// buffer
+				printWriter.println();
+				printWriter.flush();
+				
+				bufferedOutputStream.write(fileData, 0, fileLength);
+				bufferedOutputStream.flush();
 			}
 		} catch (FileNotFoundException fnex) {
-			// inform client file doesn't exist
-			fileNotFound(printWriter, fileRequested);
+			/* file doesn't exist message */
+			printWriter.println("HTTP/1.0 404 File Not Found");
+			printWriter.println("Server: Java HTTP Server 1.0");
+			printWriter.println("Date: " + new Date());
+			printWriter.println("Content-Type: text/html");
+			printWriter.println();
+			printWriter.println("<HTML>");
+			printWriter.println("<HEAD><TITLE>File Not Found</TITLE></HEAD>");
+			printWriter.println("<BODY>");
+			printWriter.println("<H2>404 File Not Found: " + requestedFile + "</H2>");
+			printWriter.println("</BODY>");
+			printWriter.println("</HTML>");
+			printWriter.flush();
 		} catch (IOException ioex) {
 			System.err.println("Server Error: " + ioex);
 		} finally {
@@ -132,21 +114,6 @@ public class WebServer extends Thread {
 			close(bufferedOutputStream); // close binary output stream
 			close(socket); // close socket connection
 		}
-	}
-
-	private void fileNotFound(PrintWriter printWriter, String file) {
-		printWriter.println("HTTP/1.0 404 File Not Found");
-		printWriter.println("Server: Java HTTP Server 1.0");
-		printWriter.println("Date: " + new Date());
-		printWriter.println("Content-Type: text/html");
-		printWriter.println();
-		printWriter.println("<HTML>");
-		printWriter.println("<HEAD><TITLE>File Not Found</TITLE></HEAD>");
-		printWriter.println("<BODY>");
-		printWriter.println("<H2>404 File Not Found: " + file + "</H2>");
-		printWriter.println("</BODY>");
-		printWriter.println("</HTML>");
-		printWriter.flush();
 	}
 
 	private String getContentType(String fileRequested) {
@@ -195,13 +162,13 @@ public class WebServer extends Thread {
 
 	public static void main(String[] args) throws IOException {
 		try {
-			ServerSocket serverConnect = new ServerSocket(DEFAULT_PORT);
-			System.out.println("\nServer started on port " + DEFAULT_PORT
-					+ "...\n");
+			ServerSocket serverConnect = new ServerSocket(DEFAULT_PORT,5,InetAddress.getByName("127.0.0.1"));
+			System.out.println("\nServer started on port " + DEFAULT_PORT + "...\n");
+			/* server will run until it's aborted */
 			while (true) {
 				WebServer webServer = new WebServer(serverConnect.accept());
 
-				// create new thread
+				/* create a new thread */
 				Thread thread = new Thread(webServer);
 				thread.start();
 			}
